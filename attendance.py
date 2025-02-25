51a1168f283fa6b6f5357d85c71fce5f04a87783
zakhar2OO3 <gordeev242424@mail.ru>
2025-02-25 15:49:07 +0100

    python simple file

---
+++ attendance.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+zakhar_table = pd.read_csv('data.csv')
+
+
+df["event_date"] = pd.to_datetime(df["event_date"])
+
+attendance_by_date = df.groupby("event_date")["is_attend"].sum()
+
+plt.figure(figsize=(10, 5))
+plt.plot(attendance_by_date.index, attendance_by_date.values, marker='o', linestyle='-', color='b')
+plt.xlabel("Дата посещений")
+plt.ylabel("Посещения")
+plt.title("Дата")
+plt.grid()
+
+plt.show()
